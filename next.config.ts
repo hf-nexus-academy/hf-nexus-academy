5c74f43641b4d2f7b110d5c409d6915188b9b67d
hf-nexus-academy <lunasmith7860@gmail.com>
2026-06-28 22:31:36 +0500

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
